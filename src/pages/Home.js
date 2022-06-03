ded54755adbaeedb40de7808dabde194ca07cff1
Lakshay <lakshay@freestand.in>
2022-06-03 23:33:49 +0530

    First commit

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (<div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorem quas dignissimos ipsum debitis provident omnis, nesciunt nobis laudantium ullam aliquid minus. Ut ullam inventore repellat. Quos nemo numquam mollitia?
+    </div>)
+}
+
+export default Home;
