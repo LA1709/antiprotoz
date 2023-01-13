deb68e6e3b7c5dace7764cc85d5e216a686d90ff
Lakshay <lakshay@freestand.in>
2023-01-13 19:36:54 +0530

    Call to fetch stats

---
+++ src/sql/fetchStats.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const fetchStats = (callback) => {
+    const data = {
+        "query": `
+        SELECT COUNT(*) FROM master;
+        SELECT COUNT(DISTINCT(Sequence)) FROM master;
+        SELECT COUNT(DISTINCT(PubmedID)) FROM master;
+        SELECT COUNT(*) FROM master WHERE Type='Free Living';
+        SELECT COUNT(*) FROM master WHERE Type='Parasitic';
+        `
+    };
+    const config = {
+        method: 'post',
+        url: 'https://3v3bd3qzhk.execute-api.us-east-2.amazonaws.com/query_sql',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data
+    };
+    axios(config).then(result => {
+        callback(result.data.map(res => Object.values(res[0])[0]))
+    }).catch(err => {
+        console.log(err);
+        callback([]);
+    })
+};
