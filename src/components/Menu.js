71f8a93626919535d6c71f986bdb081e9b0f17f4
Lakshay <lakshay@freestand.in>
2023-01-24 01:09:46 +0530

    Added dropdown

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as DownIcon } from '../assets/chevron-down.svg';
+import { ReactComponent as UpIcon } from '../assets/chevron-up.svg';
 import './menu.scss';
 
 const Menu = () => {
     const selected = window.location.pathname;
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return <div className='menu-wrapper'>
         <Link to="/">
             <div className="logo-container">
@@ -13,7 +19,21 @@ const Menu = () => {
         <div className="links-wrapper">
             <Link to="/browse" className={selected === '/browse' ? 'disabled' : ''}>Browse</Link>
             <Link to="/search" className={selected === '/search' ? 'disabled' : ''}>Search</Link>
-            <Link to="/tools" className={selected.match('/tools/') ? 'disabled' : ''}>Tools</Link>
+            <div
+                className={`dropdown${(isOpen || selected.match('/tools/')) ? " opened" : ""}`}
+                onClick={() => setIsOpen(prev => !prev)}
+            >Tools{isOpen ?
+                <UpIcon stroke="#382119" />
+                : <DownIcon stroke="#382119" />}
+            </div>
+            {isOpen && <div className="options">
+                <Link to="/tools/blast" className={selected === '/tools/blast' ? 'disabled' : ''}>
+                    BLAST Search
+                </Link>
+                <Link to="/tools/composition" className={selected === '/tools/composition' ? 'disabled' : ''}>
+                    Protein composition
+                </Link>
+            </div>}
             <Link to="/overview" className={selected === '/overview' ? 'disabled' : ''}>Data Visualisation</Link>
             <Link to="/submit" className={selected === '/submit' ? 'disabled' : ''}>Data Submission</Link>
             <Link to="/guide" className={selected === '/guide' ? 'disabled' : ''}>Guide</Link>
