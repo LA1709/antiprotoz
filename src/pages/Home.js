86bee51fbd582ac3746cd1183a2eee92057f6023
Lakshay <lakshay@freestand.in>
2023-03-22 15:23:27 +0530

    Removed comment

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchStats } from '../sql/fetchStats';
-// import Logo from '../assets/logo.png';
 import './home.scss';
 
 const Home = () => {
